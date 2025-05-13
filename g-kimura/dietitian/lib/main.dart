da845dbab64a3475e3c54cac65d6abcb14209402
kmr-gks <82916941+kmr-gks@users.noreply.github.com>
2025-05-13 18:47:32 +0900

    カメラページに変更
    javascript,iosシミュレーターでは動いた

--- g-kimura/dietitian/lib/main.dart
+++ g-kimura/dietitian/lib/main.dart
@@ -43,8 +43,8 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      //home: CameraPage(),
-      home: UploadTestPage(),
+      home: CameraPage(),
+      //home: UploadTestPage(),
     );
   }
 }
